3c51ee644d0d7df5259c83d1139bf8fbaef43481
fiddlerwoaroof <fiddlerwoaroof@gmail.com>
2016-02-12 10:15:05 -0500

    Updating README, documenting functions and macros
    - Minor change to the mustache-view macro to ensure the proper order of
      evaluation.

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple web framework that wraps ningle <8arrow.org/ningle> with a bit of
+syntactic sugar to make life easier. See server-test.lisp in </fiddlerwoaroof/alimenta>
+for an example of the usage of this library.
--- araneus.lisp
+++ araneus.lisp
@@ -2,15 +2,28 @@
 ;; views from controllers and make life more interesting.
 (in-package :araneus)
 
-(defgeneric controller (name params &key))
-(defgeneric view (name model))
-(defgeneric run-route (name params &rest r))
+(defgeneric run-route (name params &rest r)
+  (:documentation "specialized on NAME with an EQL-specializer. This generic
+                   function defines the way a specific route is to be processed"))
+
+(defgeneric controller (name params &key)
+  (:documentation "specialized on NAME with an EQL-specializer. This generic function
+                   picks out the model that the view renders for the user. Normally,
+                   this is specialized using the DEFINE-CONTROLLER macro."))
+
+(defgeneric view (name model)
+  (:documentation "specialized on NAME with an EQL-specializer. This generic function
+                   renders the model picked out by the controller. Normally, this is 
+                   specialized using the DEFINE-VIEW macr"))
 
 (defmacro setf1 (&body body)
-  "Make setf a bit nicer"
+  "Make setf a bit nicer to use with paredit"
   (list* 'setf (apply #'append body)))
 
 (defmacro defroutes (app &body routes)
+  "Define a set of routes for given paths. the ROUTES parameter expects this format:
+   ((\"/path/to/{route}\" :method :POST) route-callback) the AS-ROUTE macro helps one
+   avoid binding function values to the route for flexibility."
   (alexandria:once-only (app)
     (list* 'setf1
            (loop for ((target &key method) callback) in routes
@@ -18,12 +31,18 @@
 
  
 (defmacro as-route (name &rest r &key &allow-other-keys)
+  "Create a lambda directing requests to the route for NAME.  This uses the
+   generic function RUN-ROUTE internally whose default implementation relies on
+   appropriate implementations of CONTROLLER and VIEW. The RUN-ROUTE method receives
+   the parameters ningle passes to other functions as a first parameter, and then it
+   receives a bunch of arguments according to the arguments passed to this macro."
   `(lambda (params) (run-route ,name params ,@r)))
 
 
 (defmethod run-route (name params &rest r)
   (view name (apply #'controller (list* name params r))))
 
+; The default controller just passes its parameters directly to the view
 (defmethod controller (name params &key)
   params)
 
@@ -54,8 +73,10 @@
                                   collect `(cons ,k ,v)))))))
 
 (defmacro mustache-view (name lambda-list template &body body)
-  (alexandria:with-gensyms (model)
-    `(define-view ,name (,model)
-       (mustache (,template ,lambda-list ,model)
-         ,@body))))
+  "Define a view that renders a mustache template."
+  (alexandria:once-only (name lambda-list)
+    (alexandria:with-gensyms (model)
+      `(define-view ,name (,model)
+         (mustache (,template ,lambda-list ,model)
+                   ,@body)))))
 
